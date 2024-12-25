96f61786467a16895be30a77ca0380df586d50be
km6293 <alstn7616@naver.com>
2024-12-26 06:06:10 +0900

    design: footer 추가가

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,3 +1,4 @@
+import Footer from '@components/layout/Footer'
 import Header from '@components/layout/Header'
 import ScrollToTopButton from '@components/layout/ScrollToTopButton'
 
@@ -11,6 +12,7 @@ export default function DomainLayout({
       <Header />
       <main>{children}</main>
       <ScrollToTopButton />
+      <Footer />
     </span>
   )
 }
---
+++ src/app/_components/layout/Footer.tsx
@@ -0,0 +1,11 @@
+export default function Footer() {
+  return (
+    <footer className="star-light14 text-bg-content-neutral-base w-full p-10">
+      <div className="w-full max-w-layout justify-self-center px-10">
+        <div className="my-4 h-px bg-content-neutral-base"></div>
+        <nav>author : 김민수</nav>
+        <nav>email : alstn7616@naver.com</nav>
+      </div>
+    </footer>
+  )
+}
