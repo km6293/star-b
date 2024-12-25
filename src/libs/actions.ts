645043a1a823885a824fe0290b5aa57d73710d94
km6293 <alstn7616@naver.com>
2024-12-26 05:08:56 +0900

    feat: 캐싱 처리

--- src/libs/actions.ts
+++ src/libs/actions.ts
@@ -1,36 +1,30 @@
-import type { LawCase, PostType } from '@libs/internalTypes'
+import type { PostType } from '@libs/internalTypes'
 import fetcher from '@libs/fetcher'
 import { QueryFunction } from '@tanstack/react-query'
 import { POST_LIMIT } from './constants'
 
-export const getSearchResult: QueryFunction<PostType, ['getSearchResult', string]> = async ({ queryKey }) => {
+export const getSearchResult: QueryFunction<PostType, ['posts', string]> = async ({ queryKey }) => {
   const [, query] = queryKey
 
   const urlSearchParams = new URLSearchParams(query)
+
   if (!urlSearchParams.has('pagination[pageSize]')) {
     urlSearchParams.set('pagination[pageSize]', POST_LIMIT.toString())
   }
+
   if (!urlSearchParams.has('pagination[page]')) {
     urlSearchParams.set('pagination[page]', '1')
   }
-
   const result = await fetcher<PostType>({
     method: 'GET',
     url: `/api/posts?${urlSearchParams}`,
     customOptions: {
       next: {
-        tags: ['getSearchResult', query]
-      }
+        tags: ['posts', urlSearchParams.toString()]
+      },
+      cache: 'force-cache'
     }
   })
 
-  return result
-}
-
-export const getLawCaseDetail = async (id: number): Promise<LawCase[]> => {
-  const result = await fetcher<LawCase[]>({
-    method: 'GET',
-    url: `/api/posts/${id}`
-  })
   return result
 }
